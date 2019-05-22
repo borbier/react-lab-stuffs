a8ce130d377c79f89c52651ddc7e464203039c06
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-05-22 22:19:29 +0700

    :sparkles: Put redux components to main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import SampleHeading from './components/SampleHeading.jsx';
 import SampleComponent1 from './components/SampleComponent1.jsx';
 import IdiotCounter from './components/IdiotCounter';
 
-function App() {
+import Action from './actions';
+import ReduxCounter from './components/ReduxCounter';
+
+function App(props) {
   return (
     <div>
       <p>Sample Components with props parsing</p>
@@ -14,8 +19,24 @@ function App() {
       <hr/>
       <p>Just an idiot counter (with React hook)</p>
       <IdiotCounter />
+      <hr/>
+      <p>Idiot counter (with React Redux)</p>
+      <ReduxCounter 
+        value={props.counter}
+        onIncrement={() => props.increment()}
+        onDecrement={() => props.decrement()}
+      />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  counter: state.counter
+})
+
+const mapDispatchtoProps = (dispatch) => ({
+  increment: () => dispatch({type: Action.INCREMENT, text: "INCREMENT Redux"}),
+  decrement: () => dispatch({type: Action.DECREMENT, text: "DECREMENT Redux"})
+})
+
+export default connect(mapStateToProps, mapDispatchtoProps)(App);
